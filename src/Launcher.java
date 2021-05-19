e2550b989f2fc811d7c67b4decfc4b5eeef32fe5
Theophile GREGOIRE <theophile.gregoire@icloud.com>
2021-05-19 11:25:55 +0200

    ajout fibo

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,13 +1,25 @@
 import java.util.Scanner;
 
 public class Launcher {
+
+    public static int fib(int n) {
+        if (n < 2)
+            return (n);
+        return (fib(n - 2) + fib(n - 1));
+    }
+
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         System.out.println("Bienvenue petit codeur");
         String inputUser = myObj.nextLine();
 
         while (!inputUser.equals("quit")) {
-            if (!inputUser.equals("quit")) {
+            if (inputUser.equals("fibo")) {
+                System.out.println("Veuillez saisir un nombre");
+                int indexFibo = myObj.nextInt();
+                myObj.nextLine();
+                System.out.println(fib(indexFibo));
+            } else {
                 System.out.println("Unknown command");
             }
             inputUser = myObj.nextLine();
